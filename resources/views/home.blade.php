fa7c946043b3e119222c2c03dd8a7f19c4267af4
Viacheslav <zhizhkoslava@gmail.com>
2019-02-16 12:29:10 +0200

    reworked products and vievs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,10 @@
         <div class="row">
             @foreach($products as $product)
                 <div class="col-md-4">
-
-                    <img class="product_img" style="width:200px; height:130px;" src="{{  $product->product_images[0]->source_img }} ">
-                    <div><a href="#">{{ $product->name }}</a></div>
+                    <img class="product_img" style="width:300px; height:200px;" src="{{  $product->product_images[0]->source_img }} ">
+                    <div><a href="products/{{ $product->id }}">{{ $product->name }}</a></div>
                     <span>{{ $product->price }}</span>
-                    <p><a class="btn btn-dark" href="#" role="button">Add to cart</a></p>
+                    <p><a class="btn btn-dark" href="products/{{ $product->id }}" role="button">Add to cart</a></p>
 
                 </div>
             @endforeach
